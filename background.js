33dbf1a294900787d9b141f961db5f6bb184ce49
Paul <78680077+Fruchtii@users.noreply.github.com>
2024-09-23 09:39:25 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,16 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "changeNumber",
+    title: "Change this number",
+    contexts: ["selection"]
+  });
+});
 
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "changeNumber") {
+    chrome.tabs.sendMessage(tab.id, {
+      action: "promptChange",
+      selection: info.selectionText
+    });
+  }
+});
